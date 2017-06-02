c5d006f1bee849cdf910e4708cbf42603fd4496c
Kathryn Williams <ksw4321@gmail.com>
2017-06-02 14:19:53 +0100

    Tidied up instance variables

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,7 @@
 class Game 
   
-  attr_reader :guess_array, :solution, :check
-  
   def initialize
     @guess_array = ["-", "-", "-", "-"]
-    @counter = 0
-    @correct_positions = 0 
   end
   
   def generate_solution
@@ -23,15 +19,15 @@ class Game
   end
   
   def make_guess 
-    @selections_made = 0 
-    @position_number = 0 
-    until @selections_made == 4
-      print "#{@position_number + 1}: "
-      @user_input = gets.chomp.upcase
-      if (@user_input.length == 1) && (@user_input =~ /[WPYGRB]/)
-        @guess_array[@position_number] = @user_input
-        @position_number += 1
-        @selections_made += 1 
+    selections_made = 0 
+    position_number = 0 
+    until selections_made == 4
+      print "#{position_number + 1}: "
+      user_input = gets.chomp.upcase
+      if (user_input.length == 1) && (user_input =~ /[WPYGRB]/)
+        @guess_array[position_number] = user_input
+        position_number += 1
+        selections_made += 1 
       else 
         puts "Invalid entry. Please try again."
       end
@@ -46,15 +42,16 @@ class Game
   end  
 
   def check_correct_positions
-    @correct_positions = 0 
+    correct_positions = 0 
     @array_position = 0 
     until @array_position == 4 
       if (@solution[@array_position] == @guess_array[@array_position])
-        @correct_positions += 1
+        correct_positions += 1
       end 
       @array_position += 1 
     end 
-    puts "#{@correct_positions} in the correct position."
+    puts "#{
+      correct_positions} in the correct position."
   end
 
   def result
@@ -68,16 +65,17 @@ class Game
   def play_game 
     selections_left = 12
     generate_solution
+    counter = 0 
     puts "Please enter your colours one at a time."
     puts "W = white, P = pink, Y = yellow, G = green, R = red, B = blue."
-      until (@counter == 12) || (@guess_array == @solution)
+      until (counter == 12) || (@guess_array == @solution)
         selections_left > 1? noun = "attempts" : noun = "attempt"
         puts "You have #{selections_left} #{noun} left"
         display_board(@guess_array)
         make_guess
         check_common_values
         check_correct_positions
-        @counter += 1 
+        counter += 1 
         selections_left -= 1
       end
       result
